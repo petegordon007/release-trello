967cabde8210a2008935cf251a10d38e502ba4cc
petegordon007 <petegordon007@gmail.com>
2014-10-07 15:18:19 +0100

    [DONE]Calculate total and done total by points and show percentage done

--- release_overview.js
+++ release_overview.js
@@ -1,11 +1,74 @@
+$(document).ready(function(){
+	Trello.authorize({
+	    interactive: true,
+	    type: "popup",
+	    expiration: "never",
+	    name: "surveyrequest",
+	    persist: "true",
+	    success: function() { onAuthorizeSuccessful(); },
+	    error: function() { onFailedAuthorization(); },
+	    scope: { read: true, write: true},
+	});	
 
+	function onAuthorizeSuccessful() {
+	    /* Trello.boards.get("51a7310f77b391ff2300077a",  function( board) {
+	    	console.log( board.name );
+	    }); */
+		$("#connectionstatus").text("Ready");
 
-var OverviewReport = {
-	authenticate: function () {
-		console.log( "I am authenticating" );
 	}
 
+	function onFailedAuthorization() {
+	    $("#connectionstatus").text("Authorisation Failed");
+	}
+});
+
+function findReleaseStories( needle ) {
+	$('#summary').empty();
+	$('#output').empty();
+	
+	var re = new RegExp( needle.toLowerCase() );
+	var rePoints = new RegExp( "\{(.*)\}" );
+	var reDoneList = new RegExp( "Done:" );
+	var total = 0;
+	var complete = 0;
+
+	Trello.get("boards/51a7310f77b391ff2300077a/lists", { fields : 'name', cards: 'open', card_fields : 'name,url' }, function(lists) {
+
+		$.each( lists, function( ix, list ) {
+			$.each( list.cards, function ( ix2, card ) {
+				if ( re.test( card.name.toLowerCase() ) ) {
+
+					$("<span>")
+					.attr({id: card.id})
+	                .addClass("card")
+	                .text( '[' + list.name + '] : ' + card.name )
+	                .appendTo('#output');
+
+	                $("<a>")
+	                .addClass("link")
+	                .attr({href: card.url, target: "trello"})
+	                .text( 'link' )
+	                .appendTo('#' + card.id);
+	           
+		            var matches = rePoints.exec( card.name );
+		            if ( matches && matches.length > 0 ) {
+		            	if ( reDoneList.test( list.name ) ) {
+		            		complete += parseInt( matches[1] );
+		            	}
+		            	total += parseInt( matches[1] );
+		            }     
+	            }
+			});
+		});
+
+		$("<div>")
+		.text("Total: " + total + " Complete: " + complete)
+		.appendTo('#summary');
+
+		$( "#progressbar" ).progressbar({
+			max: total, 
+      		value: complete
+    	});
+    });
 };
-
-var overview = Object.create( OverviewReport );
-
